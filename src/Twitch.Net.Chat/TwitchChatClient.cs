8612d14347699c17bab2a8eea6b4a65b8effe35b
Aux <justin.gentry@auxesis.tv>
2016-12-07 10:37:44 -0600

    Organized folders a little

---
+++ src/Twitch.Net.Chat/TwitchChatClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Twitch.Net.Chat
+{
+    public class TwitchChatClient : ITwitchClient
+    {
+
+    }
+}
---
+++ src/Twitch.Net.Rest/TwitchRestClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Twitch.Net.Rest
+{
+    public class TwitchRestClient : ITwitchClient
+    {
+
+    }
+}
