0031bbc2de3f609b594a25ce948fa5d2d237f71d
Aux <justin.gentry@auxesis.tv>
2016-12-08 09:37:12 -0600

    Singularized Scopes -> Scope

--- src/Twitch.Net.Rest/Entities/Scopes.cs
+++ src/Twitch.Net.Rest/Entities/Scopes.cs
@@ -1,30 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Twitch.Rest
+﻿namespace Twitch.Rest
 {
-    public sealed class Scopes
+    public sealed class Scope
     {
         private readonly string _name;
 
-        public static readonly Scopes UserRead = new Scopes("user_read");
-        public static readonly Scopes UserBlocksEdit = new Scopes("user_blocks_edit");
-        public static readonly Scopes UserBlocksRead = new Scopes("user_blocks_read");
-        public static readonly Scopes UserFollowsEdit = new Scopes("user_follows_edit");
-        public static readonly Scopes ChannelRead = new Scopes("channel_read");
-        public static readonly Scopes ChannelEditor = new Scopes("channel_editor");
-        public static readonly Scopes ChannelCommercial = new Scopes("channel_commercial");
-        public static readonly Scopes ChannelStream = new Scopes("channel_stream");
-        public static readonly Scopes ChannelSubscriptions = new Scopes("channel_subscriptions");
-        public static readonly Scopes UserSubscriptions = new Scopes("user_subscriptions");
-        public static readonly Scopes ChannelCheckSubscriptions = new Scopes("channel_check_subscription");
-        public static readonly Scopes ChatLogin = new Scopes("chat_login");
-        public static readonly Scopes ChannelFeedRead = new Scopes("channel_feed_read");
-        public static readonly Scopes ChannelFeedEdit = new Scopes("channel_feed_edit");
+        public static readonly Scope UserRead = new Scope("user_read");
+        public static readonly Scope UserBlocksEdit = new Scope("user_blocks_edit");
+        public static readonly Scope UserBlocksRead = new Scope("user_blocks_read");
+        public static readonly Scope UserFollowsEdit = new Scope("user_follows_edit");
+        public static readonly Scope ChannelRead = new Scope("channel_read");
+        public static readonly Scope ChannelEditor = new Scope("channel_editor");
+        public static readonly Scope ChannelCommercial = new Scope("channel_commercial");
+        public static readonly Scope ChannelStream = new Scope("channel_stream");
+        public static readonly Scope ChannelSubscriptions = new Scope("channel_subscriptions");
+        public static readonly Scope UserSubscriptions = new Scope("user_subscriptions");
+        public static readonly Scope ChannelCheckSubscription = new Scope("channel_check_subscription");
+        public static readonly Scope ChatLogin = new Scope("chat_login");
+        public static readonly Scope ChannelFeedRead = new Scope("channel_feed_read");
+        public static readonly Scope ChannelFeedEdit = new Scope("channel_feed_edit");
 
-        private Scopes(string name)
+        private Scope(string name)
         {
             _name = name;
         }
