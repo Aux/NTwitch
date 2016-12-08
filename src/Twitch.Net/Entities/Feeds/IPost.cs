b0af0d785e893ee91221ef565be4f7c8df147497
Aux <justin.gentry@auxesis.tv>
2016-12-08 15:28:34 -0600

    Why did this get skipped

---
+++ src/Twitch.Net/Entities/Feeds/IPost.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Twitch
+{
+    public interface IPost
+    {
+        DateTime CreatedAt { get; }
+
+        uint Id { get; }
+        IPostUser Author { get; }
+        string Body { get; }
+        string[] Emotes { get; }
+        IEnumerable<PostReaction> Reactions { get; }
+        bool IsDeleted { get; }
+
+        Task DeleteAsync();
+        Task CreateReactionAsync(uint id);
+        Task DeleteReactionAsync(uint id);
+    }
+}
