4c0d326466272457d87b99477629382c9d22b2d8
Aux <justin.gentry@auxesis.tv>
2016-12-08 08:47:50 -0600

    Added EnsureHttpClientExists
    RestApiClient no longer creates a new instance of httpclient for every
    request.

--- src/Twitch.Net.Rest/API/RestApiClient.cs
+++ src/Twitch.Net.Rest/API/RestApiClient.cs
@@ -5,8 +5,9 @@ using System.Threading.Tasks;
 
 namespace Twitch.Rest
 {
-    internal class RestApiClient
+    internal class RestApiClient : IDisposable
     {
+        private HttpClient _http;
         private string _clientid;
         private string _baseurl;
 
@@ -16,47 +17,55 @@ namespace Twitch.Rest
             _baseurl = baseurl;
         }
 
-        internal async Task SendAsync(string method, string endpoint, object payload)
+        internal void EnsureHttpClientCreated()
         {
-            using (var http = new HttpClient())
+            if (_http == null)
             {
+                var http = new HttpClient();
+
                 http.BaseAddress = new Uri(_baseurl);
                 http.DefaultRequestHeaders.Add("Accept", "application/vnd.twitchtv.v2+json");
                 http.DefaultRequestHeaders.Add("Client-ID", _clientid);
 
-                var request = new HttpRequestMessage(new HttpMethod(method), endpoint);
-
-                var response = await http.SendAsync(request);
-                if (!response.IsSuccessStatusCode)
-                    throw new HttpRequestException($"{(int)response.StatusCode}: {response.ReasonPhrase}");
+                _http = http;
             }
+        }
+
+        internal async Task SendAsync(string method, string endpoint, object payload)
+        {
+            EnsureHttpClientCreated();
+            var request = new HttpRequestMessage(new HttpMethod(method), endpoint);
+
+            var response = await _http.SendAsync(request);
+            if (!response.IsSuccessStatusCode)
+                throw new HttpRequestException($"{(int)response.StatusCode}: {response.ReasonPhrase}");
         }
 
         internal async Task<T> SendAsync<T>(string method, string endpoint, object payload)
         {
-            using (var http = new HttpClient())
+            EnsureHttpClientCreated();
+            var request = new HttpRequestMessage(new HttpMethod(method), endpoint);
+
+            var response = await _http.SendAsync(request);
+            if (!response.IsSuccessStatusCode)
             {
-                http.BaseAddress = new Uri(_baseurl);
-                http.DefaultRequestHeaders.Add("Accept", "application/vnd.twitchtv.v2+json");
-                http.DefaultRequestHeaders.Add("Client-ID", _clientid);
-
-                var request = new HttpRequestMessage(new HttpMethod(method), endpoint);
-
-                var response = await http.SendAsync(request);
-                if (!response.IsSuccessStatusCode)
-                {
-                    throw new HttpRequestException($"{(int)response.StatusCode}: {response.ReasonPhrase}");
-                } else
-                {
-                    string content = await response.Content.ReadAsStringAsync();
-                    return JsonConvert.DeserializeObject<T>(content);
-                }
+                throw new HttpRequestException($"{(int)response.StatusCode}: {response.ReasonPhrase}");
+            }
+            else
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<T>(content);
             }
         }
 
         internal async Task LoginAsync()
         {
             await Task.Delay(1);
+        }
+
+        public void Dispose()
+        {
+            _http.Dispose();
         }
     }
 }
