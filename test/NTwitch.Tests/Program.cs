0b4c39ee2cc69306399efd8f3c3e34fc7d0eba90
Aux <justin.gentry@auxesis.tv>
2016-12-16 11:56:23 -0600

    RestClient tests

--- test/NTwitch.Tests/Program.cs
+++ test/NTwitch.Tests/Program.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using NTwitch.Rest;
+using System;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace NTwitch.Test
@@ -8,7 +8,31 @@ namespace NTwitch.Test
     public class Program
     {
         public static void Main(string[] args)
+            => new Program().Start().GetAwaiter().GetResult();
+
+        private TwitchRestClient _client;
+
+        public async Task Start()
         {
+            string clientid = "";
+
+            _client = new TwitchRestClient();
+
+            await _client.LoginAsync(clientid);
+
+            while (true)
+            {
+                Console.Write($"Find User: ");
+                string search = Console.ReadLine();
+                var user = await _client.GetUserAsync(search);
+
+                var properties = user.GetType().GetProperties();
+
+                foreach (var p in properties)
+                {
+                    Console.WriteLine($"{p.Name}: {p.GetValue(user)}");
+                }
+            }
         }
     }
 }
