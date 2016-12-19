814ec55dca4303e03d8301c8bfd86abc683229e7
Aux <justin.gentry@auxesis.tv>
2016-12-19 15:30:50 -0600

    Add logging objects

---
+++ src/NTwitch.Core/Utility/LogLevel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NTwitch
+{
+    public enum LogLevel
+    {
+        Critical,
+        Errors,
+        Warnings,
+        Info,
+        Debug
+    }
+}
