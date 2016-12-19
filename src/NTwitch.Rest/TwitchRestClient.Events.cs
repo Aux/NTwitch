bf49f54024ed63784843df2e96d724c741d0047d
Aux <justin.gentry@auxesis.tv>
2016-12-19 15:32:19 -0600

    Begin adding restclient log event

---
+++ src/NTwitch.Rest/TwitchRestClient.Events.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace NTwitch.Rest
+{
+    public partial class TwitchRestClient
+    {
+        private readonly AsyncEvent<Func<LogMessage, Task>> _logEvent = new AsyncEvent<Func<LogMessage, Task>>();
+        public event Func<LogMessage, Task> Log
+        {
+            add { _logEvent.Add(value); }
+            remove { _logEvent.Remove(value); }
+        }
+    }
+}
