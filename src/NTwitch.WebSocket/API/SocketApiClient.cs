cd48ce5b9c1a85ef7b346cc136468ab2f184e793
Aux <justin.gentry@auxesis.tv>
2016-12-26 13:04:31 -0600

    Add SocketState

--- src/NTwitch.WebSocket/API/SocketApiClient.cs
+++ src/NTwitch.WebSocket/API/SocketApiClient.cs
@@ -4,12 +4,22 @@ namespace NTwitch.WebSocket
 {
     internal class SocketApiClient : IDisposable
     {
+        private LogManager _log;
+        private SocketState _state;
+        private string _baseurl;
+        private string _token;
+        private int _port;
 
-
+        public SocketApiClient(LogManager manager, string baseurl, int port = 11000)
+        {
+            _log = manager;
+            _baseurl = baseurl;
+            _port = port;
+        }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            _state.Dispose();
         }
     }
 }
---
+++ src/NTwitch.WebSocket/API/SocketState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace NTwitch.WebSocket
+{
+    public class SocketState : IDisposable
+    {
+        public int BufferSize { get; set; }
+        public byte[] Buffer { get; set; }
+        public StringBuilder Builder { get; set; } = new StringBuilder();
+        public Socket Socket = null;
+
+        public SocketState(int buffersize = 256)
+        {
+            BufferSize = buffersize;
+            Buffer = new byte[BufferSize];
+        }
+
+        public void Dispose()
+        {
+            Socket.Dispose();
+        }
+    }
+}
