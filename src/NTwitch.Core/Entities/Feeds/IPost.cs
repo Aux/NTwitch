b03d242f27b0abc96b39abeb9b30ff09df74868a
Aux <justin.gentry@auxesis.tv>
2016-12-28 10:47:49 -0600

    Remove set from feed entities

--- src/NTwitch.Core/Entities/Feeds/IPost.cs
+++ src/NTwitch.Core/Entities/Feeds/IPost.cs
@@ -5,13 +5,13 @@ namespace NTwitch
 {
     public interface IPost : IEntity
     {
-        string Body { get; set; }
-        DateTime CreatedAt { get; set; }
-        bool IsDeleted { get; set; }
-        IEnumerable<IEmbed> Embeds { get; set; }
-        IEnumerable<IEmote> Emotes { get; set; }
-        IPostPermissions Permissions { get; set; }
-        IEnumerable<IPostReaction> Reactions { get; set; }
+        string Body { get; }
+        DateTime CreatedAt { get; }
+        bool IsDeleted { get; }
+        IEnumerable<IEmbed> Embeds { get; }
+        IEnumerable<IEmote> Emotes { get; }
+        IPostPermissions Permissions { get; }
+        IEnumerable<IPostReaction> Reactions { get; }
         IUser User { get; set; }
     }
 }
--- src/NTwitch.Core/Entities/Feeds/IPostReaction.cs
+++ src/NTwitch.Core/Entities/Feeds/IPostReaction.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace NTwitch.Core.Entities.Feeds
+namespace NTwitch
 {
     public class IPostReaction
     {
