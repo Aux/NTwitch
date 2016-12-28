8673d40adfb5ef5b05264a853a983956761d687b
Aux <justin.gentry@auxesis.tv>
2016-12-28 10:11:14 -0600

    Add game objects

---
+++ src/NTwitch.Core/Entities/Games/IGame.cs
@@ -0,0 +1,11 @@
+﻿namespace NTwitch
+{
+    public interface IGame : IEntity
+    {
+        ulong GiantbombId { get; }
+        string Name { get; }
+        int Popularity { get; }
+        TwitchImage Box { get; }
+        TwitchImage Logo { get; }
+    }
+}
---
+++ src/NTwitch.Core/Entities/Games/ITopGame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NTwitch
+{
+    public interface ITopGame : IEntity
+    {
+        int Channels { get; }
+        int Viewers { get; }
+        IGame Game { get; }
+    }
+}
