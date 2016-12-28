77c42b04b09b7896eb68eefee76b008079b92f50
Aux <justin.gentry@auxesis.tv>
2016-12-28 10:48:24 -0600

    Add Rest feed entities

---
+++ src/NTwitch.Rest/Entities/Feeds/RestPost.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NTwitch.Rest
+{
+    public class RestPost : RestEntity, IPost
+    {
+    }
+}
