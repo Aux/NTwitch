766543a80c79b7d12b39c2a1982e88f1620d1fbc
Aux <justin.gentry@auxesis.tv>
2016-12-29 15:26:22 -0600

    Add RestPostComment object

--- src/NTwitch.Rest/Entities/Feeds/RestPostComment.cs
+++ src/NTwitch.Rest/Entities/Feeds/RestPostComment.cs
@@ -1,11 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
-namespace NTwitch.Rest.Entities.Feeds
+namespace NTwitch.Rest
 {
-    public class RestPostComment
+    public class RestPostComment : RestEntity, IPostComment
     {
+        public string Body { get; }
+        public DateTime CreatedAt { get; }
+        public IEnumerable<IEmote> Emotes { get; }
+        public bool IsDeleted { get; }
+        public IPostPermissions Permissions { get; }
+        public IEnumerable<IPostReaction> Reactions { get; }
+        public IUser User { get; }
+        
+        public RestPostComment(TwitchRestClient client, ulong id) : base(client, id) { }
+
+        public Task CreateReactionAsync(ulong emoteid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteReactionAsync(ulong emoteid)
+        {
+            throw new NotImplementedException();
+        }
+
+        //IPostComment
+        IEnumerable<IEmote> IPostComment.Emotes
+            => Emotes;
+        IPostPermissions IPostComment.Permissions
+            => Permissions;
+        IEnumerable<IPostReaction> IPostComment.Reactions
+            => Reactions;
+        IUser IPostComment.User
+            => User;
     }
 }
