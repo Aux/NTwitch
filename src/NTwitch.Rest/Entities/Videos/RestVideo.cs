b1bad6162a2204702ec41247f07a0a5eb0cae5e1
Aux <justin.gentry@auxesis.tv>
2016-12-29 14:37:41 -0600

    Add RestVideo object

---
+++ src/NTwitch.Rest/Entities/Videos/RestVideo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NTwitch.Rest
+{
+    public class RestVideo : RestEntity, IVideo
+    {
+        public ulong BroadcastId { get; }
+        public RestChannelSummary Channel { get; }
+        public DateTime CreatedAt { get; }
+        public string Description { get; }
+        public string DescriptionRaw { get; }
+        public string Game { get; }
+        public string Language { get; }
+        public int Length { get; }
+        public TwitchImage Preview { get; }
+        public DateTime PublishedAt { get; }
+        public string Status { get; }
+        public string[] Tags { get; }
+        public string Title { get; }
+        public BroadcastType Type { get; }
+        public string Url { get; }
+        public string Viewable { get; }
+        public int Views { get; }
+
+        public RestVideo(TwitchRestClient client, ulong id) : base(client, id) { }
+
+        //IVideo
+        IChannelSummary IVideo.Channel
+            => Channel;
+    }
+}
