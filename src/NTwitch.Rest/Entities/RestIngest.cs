7813a686a6f309798a6793c7f9f390d763ee3cf1
Aux <justin.gentry@auxesis.tv>
2016-12-29 14:17:24 -0600

    Add RestIngest object

---
+++ src/NTwitch.Rest/Entities/RestIngest.cs
@@ -0,0 +1,12 @@
+﻿namespace NTwitch.Rest
+{
+    public class RestIngest : RestEntity, IIngest
+    {
+        public double Availability { get; }
+        public bool IsDefault { get; }
+        public string Name { get; }
+        public string UrlTemplate { get; }
+
+        public RestIngest(TwitchRestClient client, ulong id) : base(client, id) { }
+    }
+}
