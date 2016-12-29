8f0d838a3d43ecfae84d871d84c6a26646985ab1
Aux <justin.gentry@auxesis.tv>
2016-12-29 14:18:12 -0600

    Add RestGame objects

---
+++ src/NTwitch.Rest/Entities/Games/RestGame.cs
@@ -0,0 +1,13 @@
+﻿namespace NTwitch.Rest
+{
+    public class RestGame : RestEntity, IGame
+    {
+        public TwitchImage Box { get; }
+        public ulong GiantbombId { get; }
+        public TwitchImage Logo { get; }
+        public string Name { get; }
+        public int Popularity { get; }
+
+        public RestGame(TwitchRestClient client, ulong id) : base(client, id) { }
+    }
+}
---
+++ src/NTwitch.Rest/Entities/Games/RestTopGame.cs
@@ -0,0 +1,15 @@
+﻿namespace NTwitch.Rest
+{
+    public class RestTopGame : RestEntity, ITopGame
+    {
+        public int Channels { get; }
+        public RestGame Game { get; }
+        public int Viewers { get; }
+
+        public RestTopGame(TwitchRestClient client, ulong id) : base(client, id) { }
+
+        //IGame
+        IGame ITopGame.Game
+            => Game;
+    }
+}
