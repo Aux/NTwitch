f6d58915e59fb9427d0f8c9f5c5807d252cd770d
Aux <justin.gentry@auxesis.tv>
2017-01-06 16:14:02 -0600

    Temporarily removed Chat project

--- src/NTwitch.Chat/TwitchChatConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NTwitch.Chat
-{
-    public class TwitchChatConfig
-    {
-        public string Host { get; set; } = "irc.chat.twitch.tv";
-        public int Port { get; set; } = 6667;
-        public LogLevel LogLevel { get; set; }
-    }
-}
