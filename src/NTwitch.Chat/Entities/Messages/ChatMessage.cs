d8745fcd7ba116eb633491e5b1c4fd149edcafb7
Aux <justin.gentry@auxesis.tv>
2017-01-12 16:15:46 -0600

    Add early message objects

---
+++ src/NTwitch.Chat/Entities/Messages/ChatMessage.cs
@@ -0,0 +1,26 @@
+﻿using NTwitch.Rest;
+using System;
+using System.Collections.Generic;
+
+namespace NTwitch.Chat
+{
+    public class ChatMessage : MessageBase
+    {
+        [ChatProperty("id")]
+        public string Id { get; private set; }
+        [ChatProperty("emotes")]
+        public IEnumerable<string> Emotes { get; private set; }
+        [ChatProperty]
+        public ChatChannel Channel { get; private set; }
+        public string Content { get; private set; }
+
+        internal ChatMessage(TwitchChatClient client) : base(client) { }
+
+        internal static ChatMessage Create(BaseRestClient client, string msg)
+        {
+            var message = new ChatMessage(client as TwitchChatClient);
+            ChatParser.PopulateObject(msg, message);
+            return message;
+        }
+    }
+}
