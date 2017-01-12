7c055a8d1adcc49b65c87136cecf0c719cae0f95
Aux <justin.gentry@auxesis.tv>
2017-01-12 10:43:57 -0600

    Add RestClient

---
+++ src/NTwitch.Rest/API/RequestOptions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace NTwitch.Rest
+{
+    public class RequestOptions
+    {
+        public int ApiVersion { get; set; } = 5;
+        public Dictionary<string, string> Parameters { get; set; } = new Dictionary<string, string>();
+        public object Payload { get; set; }
+    }
+}
