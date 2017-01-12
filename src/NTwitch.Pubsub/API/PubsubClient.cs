716583a3ef83b75bc4988049a1aaffd8bd34075c
Aux <justin.gentry@auxesis.tv>
2017-01-12 14:45:55 -0600

    Some beginning attempts at pubsub socket client

--- src/NTwitch.Pubsub/API/PubsubClient.cs
+++ src/NTwitch.Pubsub/API/PubsubClient.cs
@@ -1,25 +1,75 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace NTwitch.Pubsub
 {
     public class PubsubClient : IDisposable
     {
+        private ClientWebSocket _client;
+        private LogManager _log;
+        private CancellationTokenSource _cancelTokenSource;
+        private Task _task;
+
+        private string _host;
         private bool _disposed;
+
+        public PubsubClient(LogManager log, string host)
+        {
+            _host = host;
+        }
+
+        public Task<string> SendAsync(string method, object payload)
+        {
+            if (_client == null)
+                throw new InvalidOperationException();
+
+            throw new NotImplementedException();
+        }
+
+        public async Task ConnectAsync()
+        {
+            _cancelTokenSource = new CancellationTokenSource();
+
+            _client = new ClientWebSocket();
+            _client.Options.Proxy = null;
+            _client.Options.KeepAliveInterval = TimeSpan.Zero;
+
+            await _client.ConnectAsync(new Uri(_host), _cancelTokenSource.Token);
+            _task = RunAsync(_cancelTokenSource.Token);
+        }
+
+        public async Task DisconnectAsync(bool disposing = false)
+        {
+            try { _cancelTokenSource.Cancel(false); } catch { }
+
+            if (!disposing)
+                await (_task ?? Task.Delay(0)).ConfigureAwait(false);
+
+            if (_client != null && _client.State == WebSocketState.Open)
+            {
+                var token = new CancellationToken();
+                if (!disposing)
+                    try { await _client.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, "", token); } catch { }
+                
+                try { _client.Dispose(); } catch { }
+                _client = null;
+            }
+        }
+
+        public Task RunAsync(CancellationToken cancelToken)
+        {
+            throw new NotImplementedException();
+        }
 
         protected virtual void Dispose(bool disposing)
         {
             if (!_disposed)
             {
                 if (disposing)
-                {
-                    // TODO: dispose managed state (managed objects).
-                }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-                // TODO: set large fields to null.
-
+                    DisconnectAsync(true).GetAwaiter().GetResult();
+                
                 _disposed = true;
             }
         }
