91e69f80ec65fe9335cb8e82d0f4294255c97978
Aux <justin.gentry@auxesis.tv>
2017-01-12 16:14:57 -0600

    Add tests project

---
+++ tests/NTwitch.Tests/Program.cs
@@ -0,0 +1,24 @@
+﻿using NTwitch;
+using NTwitch.Chat;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+        => new Program().Start().GetAwaiter().GetResult();
+
+    private TwitchChatClient _client;
+
+    public async Task Start()
+    {
+        _client = new TwitchChatClient(new TwitchChatConfig()
+        {
+            LogLevel = LogLevel.Info
+        });
+
+        _client.MessageReceived += OnMessageReceived;
+
+        await _client.ConnectAsync();
+        await _client.LoginAsync("datdoggo", "");
+    }
+}
