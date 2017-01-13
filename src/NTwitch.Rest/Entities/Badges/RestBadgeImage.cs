f470ef7cce3240ec6334843656ba981a7af0a952
Aux <justin.gentry@auxesis.tv>
2017-01-12 19:33:20 -0600

    Add rest badge entities

---
+++ src/NTwitch.Rest/Entities/Badges/RestBadgeImage.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace NTwitch.Rest
+{
+    public class RestBadgeImage
+    {
+        [JsonProperty("alpha")]
+        public string AlphaUrl { get; internal set; }
+        [JsonProperty("image")]
+        public string ImageUrl { get; internal set; }
+        [JsonProperty("svg")]
+        public string SvgUrl { get; internal set; }
+    }
+}
