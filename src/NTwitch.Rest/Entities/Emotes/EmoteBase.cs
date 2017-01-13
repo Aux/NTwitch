485dd4877895c49099538f4a20288f3e19f1df86
Aux <justin.gentry@auxesis.tv>
2017-01-12 19:33:14 -0600

    Add rest emote entities

---
+++ src/NTwitch.Rest/Entities/Emotes/EmoteBase.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace NTwitch.Rest
+{
+    public class EmoteBase
+    {
+        internal BaseRestClient Client { get; }
+        [JsonProperty("_id")]
+        public ulong Id { get; private set; }
+
+        internal EmoteBase(BaseRestClient client)
+        {
+            Client = client;
+        }
+    }
+}
---
+++ src/NTwitch.Rest/Entities/Emotes/RestEmoteSet.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace NTwitch.Rest
+{
+    public class RestEmoteSet : EmoteBase
+    {
+        [JsonProperty("")]
+        public IEnumerable<RestEmote> Emotes { get; internal set; }
+
+        public RestEmoteSet(TwitchRestClient client) : base(client) { }
+
+        public static RestEmoteSet Create(BaseRestClient client, string json)
+        {
+            var emote = new RestEmoteSet(client as TwitchRestClient);
+            JsonConvert.PopulateObject(json, emote);
+            return emote;
+        }
+    }
+}
