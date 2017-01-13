340f20231e890c6616e8d6e348a31ccb3e5f8b27
Aux <justin.gentry@auxesis.tv>
2017-01-12 20:33:54 -0600

    Add rest feed entities

---
+++ src/NTwitch.Rest/Entities/Feeds/PostHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NTwitch.Rest
+{
+    internal static class PostHelper
+    {
+        public static Task<RestPostComment> CreateCommentAsync(PostBase postBase, string content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Task CreateReactionAsync(PostBase postBase, ulong emoteid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Task<RestPost> DeleteAsync(PostBase postBase)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Task<RestPostComment> DeleteCommentAsync(PostBase postBase, ulong commentid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Task<IEnumerable<RestPostComment>> GetCommentsAsync(PostBase postBase, PageOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Task<RestPostComment> DeleteReactionAsync(PostBase postBase, ulong emoteid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/NTwitch.Rest/Entities/Feeds/RestPostReaction.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace NTwitch.Rest
+{
+    public class RestPostReaction
+    {
+        [JsonProperty("")]
+        public int Count { get; internal set; }
+        [JsonProperty("")]
+        public string EmoteName { get; internal set; }
+        [JsonProperty("")]
+        public string Name { get; internal set; }
+        [JsonProperty("")]
+        public ulong[] UserIds { get; internal set; }
+    }
+}
