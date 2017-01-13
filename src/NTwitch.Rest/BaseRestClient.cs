12f59b7abaa95e0a377de237c5036d966282831c
Aux <justin.gentry@auxesis.tv>
2017-01-12 20:35:03 -0600

    Update client helper

--- src/NTwitch.Rest/BaseRestClient.cs
+++ src/NTwitch.Rest/BaseRestClient.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace NTwitch.Rest
@@ -22,6 +22,57 @@ namespace NTwitch.Rest
             _rest = new RestClient(_log, _resthost, clientid, token);
             return Task.CompletedTask;
         }
+
+        public Task<RestSelfUser> GetCurrentUserAsync()
+            => ClientHelper.GetCurrentUserAsync(this);
+
+        public Task<RestSelfChannel> GetCurrentChannelAsync()
+            => ClientHelper.GetCurrentChannelAsync(this);
+
+        public Task<RestChannel> GetChannelAsync(ulong id)
+            => ClientHelper.GetChannelAsync(this, id);
+
+        public Task<IEnumerable<RestTopGame>> GetTopGamesAsync(PageOptions options = null)
+            => ClientHelper.GetTopGamesAsync(this, options);
+
+        public Task<IEnumerable<RestIngest>> GetIngestsAsync()
+            => ClientHelper.GetIngestsAsync(this);
+
+        public Task<IEnumerable<RestChannel>> FindChannelsAsync(string query, PageOptions options = null)
+            => ClientHelper.FindChannelsAsync(this, query, options);
+
+        public Task<IEnumerable<RestGame>> FindGamesAsync(string query, bool islive = true)
+            => ClientHelper.FindGamesAsync(this, query, islive);
+
+        public Task<RestStream> GetStreamAsync(ulong id, StreamType type = StreamType.All)
+            => ClientHelper.GetStreamAsync(this, id, type);
+
+        public Task<IEnumerable<RestStream>> FindStreamsAsync(string query, bool hls = true, PageOptions options = null)
+            => ClientHelper.FindStreamsAsync(this, query, hls, options);
+
+        public Task<IEnumerable<RestStream>> GetStreamsAsync(string game = null, ulong[] channelids = null, string language = null, StreamType type = StreamType.All, PageOptions options = null)
+            => ClientHelper.GetStreamsAsync(this, game, channelids, language, type, options);
+
+        public Task<IEnumerable<RestFeaturedStream>> GetFeaturedStreamsAsync(PageOptions options = null)
+            => ClientHelper.GetFeaturedStreamsAsync(this, options);
+
+        public Task<RestStreamSummary> GetStreamSummaryAsync(string game)
+            => ClientHelper.GetStreamSummaryAsync(this, game);
+
+        public Task<IEnumerable<RestTeamSummary>> GetTeamsAsync(PageOptions options = null)
+            => ClientHelper.GetTeamsAsync(this, options);
+
+        public Task<RestTeam> GetTeamAsync(string name)
+            => ClientHelper.GetTeamAsync(this, name);
+
+        public Task<RestUser> GetUserAsync(ulong id)
+            => ClientHelper.GetUserAsync(this, id);
+
+        public Task<IEnumerable<RestUser>> FindUsersAsync(string name)
+            => ClientHelper.FindUsersAsync(this, name);
+
+        public Task<IEnumerable<RestVideo>> GetTopVideosAsync(string game = null, VideoPeriod period = VideoPeriod.Week, BroadcastType type = BroadcastType.Highlight, PageOptions options = null)
+            => ClientHelper.GetTopVideosAsync(this, game, period, type, options);
 
         Task ITwitchClient.ConnectAsync()
             => Task.CompletedTask;
--- src/NTwitch.Rest/ClientHelper.cs
+++ src/NTwitch.Rest/ClientHelper.cs
@@ -1,7 +1,94 @@
-﻿namespace NTwitch.Rest
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NTwitch.Rest
 {
     internal static class ClientHelper
     {
+        public static Task<RestStream> GetStreamAsync(BaseRestClient client, ulong id, StreamType all)
+        {
+            throw new NotImplementedException();
+        }
 
+        public static Task<RestSelfUser> GetCurrentUserAsync(BaseRestClient client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Task<RestSelfChannel> GetCurrentChannelAsync(BaseRestClient client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Task<RestChannel> GetChannelAsync(BaseRestClient client, ulong id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Task<IEnumerable<RestTopGame>> GetTopGamesAsync(BaseRestClient client, PageOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Task<IEnumerable<RestIngest>> GetIngestsAsync(BaseRestClient client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Task<IEnumerable<RestGame>> FindGamesAsync(BaseRestClient client, string query, bool islive)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Task<IEnumerable<RestChannel>> FindChannelsAsync(BaseRestClient client, string query, PageOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Task<IEnumerable<RestStream>> FindStreamsAsync(BaseRestClient client, string query, bool hls, PageOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Task<IEnumerable<RestVideo>> GetTopVideosAsync(BaseRestClient client, string game, VideoPeriod period, BroadcastType type, PageOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Task<IEnumerable<RestUser>> FindUsersAsync(BaseRestClient client, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Task<IEnumerable<RestStream>> GetStreamsAsync(BaseRestClient client, string game, ulong[] channelids, string language, StreamType type, PageOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Task<IEnumerable<RestFeaturedStream>> GetFeaturedStreamsAsync(BaseRestClient client, PageOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Task<IEnumerable<RestTeamSummary>> GetTeamsAsync(BaseRestClient client, PageOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Task<RestUser> GetUserAsync(BaseRestClient client, ulong id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Task<RestTeam> GetTeamAsync(BaseRestClient client, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Task<RestStreamSummary> GetStreamSummaryAsync(BaseRestClient client, string game)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
