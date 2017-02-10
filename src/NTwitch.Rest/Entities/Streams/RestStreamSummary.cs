486cac8602f1f2b802a5f30aef6b7fa6a2458008
Aux <justin.gentry@auxesis.tv>
2017-02-10 16:11:39 -0600

    Is no longer an entity

--- src/NTwitch.Rest/Entities/Streams/RestStreamSummary.cs
+++ src/NTwitch.Rest/Entities/Streams/RestStreamSummary.cs
@@ -2,20 +2,11 @@
 
 namespace NTwitch.Rest
 {
-    public class RestStreamSummary : RestEntity
+    public class RestStreamSummary
     {
         [JsonProperty("channels")]
         public int Channels { get; private set; }
         [JsonProperty("viewers")]
         public int Viewers { get; private set; }
-
-        public RestStreamSummary(BaseRestClient client) : base(client) { }
-
-        public static RestStreamSummary Create(BaseRestClient client, string json)
-        {
-            var stream = new RestStreamSummary(client);
-            JsonConvert.PopulateObject(json, stream);
-            return stream;
-        }
     }
 }
