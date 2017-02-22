6eea771074dd258116c04eaf7ee6e00ee03aff14
Aux <justin.gentry@auxesis.tv>
2017-02-22 13:54:57 -0600

    wew what a mess

--- tests/NTwitch.Tests/Program.cs
+++ tests/NTwitch.Tests/Program.cs
@@ -1,8 +1,6 @@
 ﻿using Newtonsoft.Json;
-using NTwitch;
-using NTwitch.Rest;
+using NTwitch.Pubsub;
 using System;
-using System.Reflection;
 using System.Threading.Tasks;
 
 namespace NTwitch.Tests
@@ -12,54 +10,28 @@ namespace NTwitch.Tests
         static void Main(string[] args)
             => new Program().StartAsync().GetAwaiter().GetResult();
 
-        private TwitchRestClient _client;
+        private TwitchPubsubClient _client;
 
         public async Task StartAsync()
         {
-            _client = new TwitchRestClient(new TwitchRestConfig()
+            _client = new TwitchPubsubClient(new TwitchPubsubConfig()
             {
                 LogLevel = LogLevel.Debug
             });
 
             _client.Log += OnLog;
-            
-            await _client.LoginAsync(TokenType.OAuth, "");
-            var obj = await _client.FindGamesAsync("overwatch", true);
 
-            string output = JsonConvert.SerializeObject(obj, Formatting.Indented);
-            Console.WriteLine(output);
+            await _client.ConnectAsync();
+
+            await _client.SubscribeAsync(new PubsubTopic("video-playback.42481140"), x =>
+            {
+                Console.WriteLine(x);
+                return Task.CompletedTask;
+            });
 
             await Task.Delay(-1);
         }
-
-        //private TwitchChatClient _client;
-
-        //public async Task StartAsync()
-        //{
-        //    _client = new TwitchChatClient(new TwitchChatConfig()
-        //    {
-        //        LogLevel = LogLevel.Debug
-        //    });
-
-        //    _client.Log += OnLog;
-        //    _client.MessageReceived += OnMessageReceived;
-
-        //    await _client.ConnectAsync();
-        //    await _client.LoginAsync("datdoggo", "");
-        //    await _client.JoinAsync("auxesistv");
-
-        //    var handler = new CommandHandler();
-        //    await handler.InitializeAsync(_client);
-
-        //    await Task.Delay(-1);
-        //}
-
-        //private Task OnMessageReceived(ChatMessage msg)
-        //{
-        //    Console.WriteLine($"[{msg.Channel.Name}] {msg.User.DisplayName}: {msg.Content}");
-        //    return Task.CompletedTask;
-        //}
-
+        
         private Task OnLog(LogMessage msg)
         {
             Console.WriteLine($"[{msg.Level}] {msg.Source}: {msg.Message}");
