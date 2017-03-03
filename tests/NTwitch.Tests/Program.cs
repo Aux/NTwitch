28adb1f62964d0b65dcdd3b1e3c343f043f6429c
Aux <justin.gentry@auxesis.tv>
2017-03-03 14:29:29 -0600

    Update for latest .net core

--- tests/NTwitch.Tests/Program.cs
+++ tests/NTwitch.Tests/Program.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json;
-using NTwitch.Chat;
+﻿using NTwitch.Chat;
 using System;
 using System.Threading.Tasks;
 
@@ -20,26 +19,13 @@ namespace NTwitch.Tests
             });
 
             _client.Log += OnLog;
-            _client.MessageReceived += OnMessageReceived;
 
             await _client.ConnectAsync();
-            await _client.LoginAsync("username", "");
+            await _client.LoginAsync("datdoggo", "");
 
-            await _client.JoinAsync("timthetatman");
-
-            //await _client.SubscribeAsync(new PubsubTopic("video-playback", 42481140), x =>
-            //{
-            //    Console.WriteLine(x.Data.Topic);
-            //    return Task.CompletedTask;
-            //});
-
+            await _client.JoinAsync("theonemanny");
+            
             await Task.Delay(-1);
-        }
-
-        private Task OnMessageReceived(ChatMessage msg)
-        {
-            Console.WriteLine($"[{msg.Channel.Name}] {msg.User.DisplayName} ({msg.User.Username}): {msg.Content}");
-            return Task.CompletedTask;
         }
 
         private Task OnLog(LogMessage msg)
