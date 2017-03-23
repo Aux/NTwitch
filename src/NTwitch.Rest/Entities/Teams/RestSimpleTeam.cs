0e089313828d69779267e06fd10149af197918e3
Aux <justin.gentry@auxesis.tv>
2017-03-23 14:06:51 -0500

    Add teams

---
+++ src/NTwitch.Rest/Entities/Teams/RestSimpleTeam.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Model = NTwitch.Rest.API.Team;
+
+namespace NTwitch.Rest
+{
+    public class RestSimpleTeam : RestEntity<ulong>
+    {
+        public string Background { get; private set; }
+        public string BannerUrl { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public string DisplayName { get; private set; }
+        public string Info { get; private set; }
+        public string LogoUrl { get; private set; }
+        public string Name { get; private set; }
+        public DateTime UpdatedAt { get; private set; }
+
+        public RestSimpleTeam(BaseRestClient client, ulong id) 
+            : base(client, id) { }
+
+        internal static RestSimpleTeam Create(BaseRestClient client, Model model)
+        {
+            var entity = new RestSimpleTeam(client, model.Id);
+            entity.Update(model);
+            return entity;
+        }
+
+        internal virtual void Update(Model model)
+        {
+            Background = model.Background;
+            BannerUrl = model.BannerUrl;
+            CreatedAt = model.CreatedAt;
+            DisplayName = model.DisplayName;
+            Info = model.Info;
+            LogoUrl = model.LogoUrl;
+            Name = model.Name;
+            UpdatedAt = model.UpdatedAt;
+        }
+
+        public async Task UpdateAsync()
+        {
+            var model = await Client.RestClient.GetTeamAsync(Name).ConfigureAwait(false);
+            Update(model);
+        }
+    }
+}
---
+++ src/NTwitch.Rest/Entities/Teams/RestTeam.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Model = NTwitch.Rest.API.Team;
+
+namespace NTwitch.Rest
+{
+    public class RestTeam : RestSimpleTeam
+    {
+        public IEnumerable<RestChannel> Channels { get; private set; }
+
+        public RestTeam(BaseRestClient client, ulong id) 
+            : base(client, id) { }
+
+        internal new static RestTeam Create(BaseRestClient client, Model model)
+        {
+            var entity = new RestTeam(client, model.Id);
+            entity.Update(model);
+            return entity;
+        }
+
+        internal override void Update(Model model)
+        {
+            base.Update(model);
+            Channels = model.Channels.Select(x =>
+            {
+                var entity = new RestChannel(Client, x.Id);
+                entity.Update(x);
+                return entity;
+            });
+        }
+    }
+}
