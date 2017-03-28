9e49a7b4fb68167d7ce57caa61f88b8b50535932
Aux <justin.gentry@auxesis.tv>
2017-03-28 09:26:54 -0500

    Add rest example
    [ci skip]

---
+++ examples/NTwitch.Examples.Rest/Program.cs
@@ -0,0 +1,45 @@
+﻿using NTwitch.Rest;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NTwitch.Examples.Rest
+{
+    class Program
+    {
+        public static void Main(string[] args)
+            => new Program().StartAsync().GetAwaiter().GetResult();
+
+        private TwitchRestClient _client = new TwitchRestClient();
+
+        public async Task StartAsync()
+        {
+            Console.Write("Please enter your client id: ");
+            string clientid = Console.ReadLine();
+
+            await _client.LoginAsync(AuthMode.ClientId, clientid);
+
+            Console.WriteLine("Login success!");
+            
+            while (true)
+            {
+                Console.WriteLine();
+                Console.Write("Enter the name of a stream: ");
+                string name = Console.ReadLine();
+
+                var user = (await _client.GetUsersAsync(name)).FirstOrDefault();
+                if (user == null)
+                {
+                    Console.WriteLine($"The user `{name}` does not exist!");
+                    continue;
+                }
+
+                var stream = await _client.GetStreamAsync(user.Id);
+                if (stream == null)
+                    Console.WriteLine($"{user.DisplayName} is not currently streaming.");
+                else
+                    Console.WriteLine($"{user.DisplayName} is streaming {stream.Game} at {stream.Channel.Url}!");
+            }
+        }
+    }
+}
