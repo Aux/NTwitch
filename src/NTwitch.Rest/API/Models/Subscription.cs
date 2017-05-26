dfedf642fe3960296922b35bbb1c1305843ba4f6
Aux <justin.gentry@auxesis.tv>
2017-05-25 21:22:23 -0500

    Subscription models updated

--- src/NTwitch.Rest/API/Models/Subscription.cs
+++ src/NTwitch.Rest/API/Models/Subscription.cs
@@ -9,8 +9,16 @@ namespace NTwitch.Rest.API
         public DateTime CreatedAt { get; set; }
         [JsonProperty("_id")]
         public string Id { get; set; }
+        [JsonProperty("sub_plan")]
+        public int SubPlan { get; set; }
+        [JsonProperty("sub_plan_name")]
+        public string SubPlanName { get; set; }
+
+        // UserSub
         [JsonProperty("user")]
         public User User { get; set; }
+
+        // ChannelSub
         [JsonProperty("channel")]
         public Channel Channel { get; set; }
     }
