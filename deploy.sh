e7fcb22d6987245ce01e0b2a4e1182967894717e
Aux <justin.gentry@auxesis.tv>
2017-05-28 16:39:05 -0500

    ehm

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
-dotnet pack "src/NTwitch/NTwitch.csproj" -c "Release" -o "../../pkgs" --no-build 
-dotnet pack "src/NTwitch.Core/NTwitch.Core.csproj" -c "Release" -o "../../pkgs" --no-build 
-dotnet pack "src/NTwitch.Rest/NTwitch.Rest.csproj" -c "Release" -o "../../pkgs" --no-build 
-dotnet nuget push "pkgs/*.nupkg" $NUGET_API_KEY -verbosity detailed
+if [[ $TRAVIS_BRANCH == 'master' ]]
+  dotnet pack "src/NTwitch/NTwitch.csproj" -c "Release" -o "../../pkgs" --no-build 
+  dotnet pack "src/NTwitch.Core/NTwitch.Core.csproj" -c "Release" -o "../../pkgs" --no-build 
+  dotnet pack "src/NTwitch.Rest/NTwitch.Rest.csproj" -c "Release" -o "../../pkgs" --no-build 
+  dotnet nuget push "pkgs/*.nupkg" $NUGET_API_KEY -verbosity detailed
+fi
