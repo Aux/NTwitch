689b0a6b338ad5a762dd4fbe980604ea9ce3e241
Aux <justin.gentry@auxesis.tv>
2017-06-06 10:43:41 -0500

    UPDDDDAATE TRAVIS>YML

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,11 @@
 
 if [ "$TRAVIS_BRANCH" = "master" ]
 then
-  dotnet pack "src/NTwitch/NTwitch.csproj" -c "Release" -o "../../pkgs" --no-build 
-  dotnet pack "src/NTwitch.Core/NTwitch.Core.csproj" -c "Release" -o "../../pkgs" --no-build 
-  dotnet pack "src/NTwitch.Rest/NTwitch.Rest.csproj" -c "Release" -o "../../pkgs" --no-build 
-  dotnet nuget push "pkgs/*.nupkg" "$NUGET_API_KEY" -verbosity detailed
+  dotnet pack "./src/NTwitch/NTwitch.csproj" -c "Release" -o "../../pkgs" --no-build 
+  dotnet pack "./src/NTwitch.Core/NTwitch.Core.csproj" -c "Release" -o "../../pkgs" --no-build 
+  dotnet pack "./src/NTwitch.Rest/NTwitch.Rest.csproj" -c "Release" -o "../../pkgs" --no-build 
+  dotnet nuget push "./pkgs/*.nupkg" "$NUGET_API_KEY" -verbosity detailed
+else
+  echo "Skipping deploy"
+  echo "This branch is not master: "$TRAVIS_BRANCH
 fi
