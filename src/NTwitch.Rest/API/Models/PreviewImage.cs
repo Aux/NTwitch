19b6c51d451c7cf79d90c4bf5b956b1b487df6e2
Aux <justin.gentry@auxesis.tv>
2017-06-08 13:52:47 -0500

    Begin entites rework

---
+++ src/NTwitch.Rest/API/Models/PreviewImage.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace NTwitch.Rest.API
+{
+    internal class PreviewImage
+    {
+        [JsonProperty("small")]
+        public string Small { get; set; }
+        [JsonProperty("medium")]
+        public string Medium { get; set; }
+        [JsonProperty("large")]
+        public string Large { get; set; }
+        [JsonProperty("template")]
+        public string Template { get; set; }
+    }
+}
