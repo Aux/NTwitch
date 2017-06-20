0ee6c4086da69d8b1b2a2d08ba90a8ff94f50799
Aux <justin.gentry@auxesis.tv>
2017-06-20 12:38:37 -0500

    Pubsub message processing blah

--- src/NTwitch.Pubsub/TwitchPubsubClient.cs
+++ src/NTwitch.Pubsub/TwitchPubsubClient.cs
@@ -1,5 +1,7 @@
 ﻿using NTwitch.Pubsub.API;
 using NTwitch.Rest;
+using System;
+using System.Collections.Concurrent;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,7 +12,7 @@ namespace NTwitch.Pubsub
     {
         private readonly SemaphoreSlim _stateLock;
         private readonly Logger _pubsubLogger;
-
+        
         internal new TwitchPubsubApiClient ApiClient => base.ApiClient as TwitchPubsubApiClient;
 
         /// <summary> Gets the current connection state of this client. </summary>
@@ -87,28 +89,32 @@ namespace NTwitch.Pubsub
 
         private async Task ProcessMessageAsync(PubsubFrame<string> msg)
         {
-            var data = msg.GetData<PubsubInData>();
-            string topic = data.Topic.Split('.').First();
+            switch (msg.Type)
+            {
+                //case "PONG":
+                //    break;
+                case "MESSAGE":
+                    var data = msg.GetData<PubsubInData>();
+                    string topic = data.Topic.Split('.').First();
 
-            if (msg.Type != "MESSAGE")
-            {
-                await _pubsubLogger.DebugAsync($"Received {msg.Type}").ConfigureAwait(false);
-                return;
-            }
-            
-            switch (topic)
-            {
-                //case "channel-bits-events-v1":
-                //    break;
-                case "channel-subscribe-events-v1":
-                    var model = data.GetMessage<Subscription>();
-                    await _subscriptionReceived.InvokeAsync(PubsubSubscription.Create(this, model));
+                    switch (topic)
+                    {
+                        //case "channel-bits-events-v1":
+                        //    break;
+                        case "channel-subscribe-events-v1":
+                            var model = data.GetMessage<Subscription>();
+                            await _subscriptionReceived.InvokeAsync(PubsubSubscription.Create(this, model));
+                            break;
+                        //case "whispers":
+                        //    break;
+                        default:
+                            await _anonymousReceivedEvent.InvokeAsync(data.Message);
+                            break;
+                    }
                     break;
-                //case "whispers":
-                //    break;
                 default:
-                    await _anonymousReceivedEvent.InvokeAsync(data.Message);
-                    break;
+                    await _pubsubLogger.DebugAsync($"Received unhandled {msg.Type}").ConfigureAwait(false);
+                    return;
             }
         }
     }
