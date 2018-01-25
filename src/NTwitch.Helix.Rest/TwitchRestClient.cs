cfa7569a012f2b167b9d83146879b2a304a7cd3f
Aux <justin.gentry@auxesis.tv>
2018-01-24 20:07:55 -0600

    Add some ease of life overloads

--- src/NTwitch.Helix.Rest/TwitchRestClient.cs
+++ src/NTwitch.Helix.Rest/TwitchRestClient.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace NTwitch.Helix.Rest
 {
@@ -26,11 +27,45 @@ namespace NTwitch.Helix.Rest
         //    base.CurrentUser = RestSelfUser.Create(this, user);
         //}
 
+        // Clips
+        public Task<RestClip> GetClipAsync(string clipId, RequestOptions options = null)
+            => ClientHelper.GetClipAsync(this, clipId, options);
+
+        // Games
+        public async Task<RestGame> GetGameAsync(ulong gameId, RequestOptions options = null)
+            => (await GetGamesAsync(gameIds: new[] { gameId }, options: options).ConfigureAwait(false)).FirstOrDefault();
+        public async Task<RestGame> GetGameAsync(string gameName, RequestOptions options = null)
+            => (await GetGamesAsync(gameNames: new[] { gameName }, options: options).ConfigureAwait(false)).FirstOrDefault();
+
+        public Task<IReadOnlyCollection<RestGame>> GetGamesAsync(params ulong[] gameIds)
+            => GetGamesAsync(gameIds, null, null);
+        public Task<IReadOnlyCollection<RestGame>> GetGamesAsync(params string[] gameNames)
+            => GetGamesAsync(null, gameNames, null);
+
+        public Task<IReadOnlyCollection<RestGame>> GetGamesAsync(ulong[] gameIds = null, string[] gameNames = null, RequestOptions options = null)
+            => ClientHelper.GetGamesAsync(this, gameIds, gameNames, options);
+        public Task<IReadOnlyCollection<RestGame>> GetTopGamesAsync(RequestOptions options = null)
+            => ClientHelper.GetTopGamesAsync(this, options);
+
         // Broadcasts
+        public async Task<RestBroadcast> GetBroadcastAsync(ulong userId, RequestOptions options = null)
+            => (await GetBroadcastsAsync(userIds: new[] { userId }, options: options).Flatten().ConfigureAwait(false)).FirstOrDefault();
+        public async Task<RestBroadcast> GetBroadcastAsync(string userName, RequestOptions options = null)
+            => (await GetBroadcastsAsync(userNames: new[] { userName }, options: options).Flatten().ConfigureAwait(false)).FirstOrDefault();
+        
         public IAsyncEnumerable<IReadOnlyCollection<RestBroadcast>> GetBroadcastsAsync(string[] communityIds = null, string[] languages = null, ulong[] userIds = null, string[] userNames = null, BroadcastType? broadcastType = null, int limit = 20, RequestOptions options = null)
             => ClientHelper.GetBroadcastsAsync(this, communityIds, languages, userIds, userNames, broadcastType, limit, options);
-        
+
         // Users
+        public async Task<RestUser> GetUserAsync(ulong userId, RequestOptions options = null)
+            => (await GetUsersAsync(userIds: new[] { userId }, options: options).ConfigureAwait(false)).FirstOrDefault();
+        public async Task<RestUser> GetUserAsync(string userName, RequestOptions options = null)
+            => (await GetUsersAsync(userNames: new[] { userName }, options: options).ConfigureAwait(false)).FirstOrDefault();
+
+        public Task<IReadOnlyCollection<RestUser>> GetUsersAsync(params ulong[] userIds)
+            => GetUsersAsync(userIds, null, null);
+        public Task<IReadOnlyCollection<RestUser>> GetUsersAsync(params string[] userNames)
+            => GetUsersAsync(null, userNames, null);
         public Task<IReadOnlyCollection<RestUser>> GetUsersAsync(ulong[] userIds = null, string[] userNames = null, RequestOptions options = null)
             => ClientHelper.GetUsersAsync(this, userIds, userNames, options);
     }
